bc2a71dbca434736f8fe540eff9bee64001bd554
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-08-30 23:31:28 +0900

    code edit

--- main.py
+++ main.py
@@ -1065,7 +1065,7 @@ if __name__ == "__main__":
     # file2 = "/Users/jonabi/Downloads/TEPA/p15_Global"
     
     # 윈도우 기준 
-    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\test_mocap/hook_right_001.csv"
+    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\Collaborate_Code\\test_mocap\\uppercut_left_001.csv"
     file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p15_Global"
  
 
@@ -1104,10 +1104,10 @@ if __name__ == "__main__":
     file1_path=file1,
     file2_dir=file2,
     analyzer=analyzer,
-    keyword="hook_right",      # 필요 시 수정
+    keyword="uppercut_left",      # 필요 시 수정
     limit=None,                   # 필요 시 숫자
-    title="hook_right",          # 시트 좌측 첫 열 제목
-    output_csv_path="p15_hook_right_similarity_matrix.csv"
+    title="uppercut_left",          # 시트 좌측 첫 열 제목
+    output_csv_path="p15_uppercut_left_similarity_matrix.csv"
     )
     
     # 'standard' | 'minmax' | None
