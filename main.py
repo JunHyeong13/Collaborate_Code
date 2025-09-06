ff97d12d9a5fe56f164fbfe072d38f878ff8bca8
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-07 02:01:15 +0900

    file extract uploads

--- main.py
+++ main.py
@@ -1065,7 +1065,7 @@ if __name__ == "__main__":
     #file2 = "/Users/jonabi/Downloads/TEPA/p19_Global"
     
     # 윈도우 기준 
-    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\hook_left_004.csv"
+    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\straight_004.csv"
     file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p22_Global"
  
 
@@ -1104,10 +1104,10 @@ if __name__ == "__main__":
     file1_path=file1,
     file2_dir=file2,
     analyzer=analyzer,
-    keyword="hook_left",      # 필요 시 수정
+    keyword="straight",      # 필요 시 수정
     limit=None,                   # 필요 시 숫자
-    title="hook_left",          # 시트 좌측 첫 열 제목
-    output_csv_path="p22_hook_left_004_similarity_matrix.csv"
+    title="straight",          # 시트 좌측 첫 열 제목
+    output_csv_path="p22_straight_004_similarity_matrix.csv"
     )
     
     # 'standard' | 'minmax' | None
