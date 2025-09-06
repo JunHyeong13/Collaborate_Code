7a0197e1b3e322e4227068b5a38f024a8cd9b96d
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-06 13:43:58 +0900

    Data extract
    Data extract

--- main.py
+++ main.py
@@ -1065,7 +1065,7 @@ if __name__ == "__main__":
     #file2 = "/Users/jonabi/Downloads/TEPA/p19_Global"
     
     # 윈도우 기준 
-    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\jap_005.csv"
+    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\hook_left_004.csv"
     file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p22_Global"
  
 
@@ -1077,10 +1077,10 @@ if __name__ == "__main__":
     # 가중치 사용자 정의 예시 (필요 시 수정)
     custom_feature_weights = {
         'position': 0.0,
-        'rotation': 0.7, ## 0.7
+        'rotation': 1.0,
         'velocity': 0.0,
         'acceleration': 0.0,
-        'joint_angles': 0.3, ## 0.3
+        'joint_angles': 0.0,
     }
 
 
@@ -1104,10 +1104,10 @@ if __name__ == "__main__":
     file1_path=file1,
     file2_dir=file2,
     analyzer=analyzer,
-    keyword="jap",      # 필요 시 수정
+    keyword="hook_left",      # 필요 시 수정
     limit=None,                   # 필요 시 숫자
-    title="jap",          # 시트 좌측 첫 열 제목
-    output_csv_path="p22_jap_005_similarity_matrix.csv"
+    title="hook_left",          # 시트 좌측 첫 열 제목
+    output_csv_path="p22_hook_left_004_similarity_matrix.csv"
     )
     
     # 'standard' | 'minmax' | None
