050cb1781cddbd9d0bdafad30fef50c0664d8677
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-08 22:27:23 +0900

    Upload the extract datas
    Upload the extract datas

--- main.py
+++ main.py
@@ -1058,18 +1058,13 @@ if __name__ == "__main__":
     # hook_right_001.csv
     # jap_001.csv
     # straight_003.csv
-
-
-    # 파일 경로 예시(수정하여 사용) # 맥북 기준
-    #file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/jap_002.csv"
-    #file2 = "/Users/jonabi/Downloads/TEPA/p04_Global"
     
-    #file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/jap_004.csv"
-    #file2 = "/Users/jonabi/Downloads/TEPA/p13_Global"
+    file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/hook_left_004.csv"
+    file2 = "/Users/jonabi/Downloads/TEPA/p05_Global"
     
     # 윈도우 기준 
-    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\straight_002.csv"
-    file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p21_Global"
+    #file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\straight_004.csv"
+    #file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p14_Global"
  
 
     # 실행 중 어떤 파일을 비교하는지 표시
@@ -1107,10 +1102,10 @@ if __name__ == "__main__":
     file1_path=file1,
     file2_dir=file2,
     analyzer=analyzer,
-    keyword="straight",      # 필요 시 수정
+    keyword="hook_left",      # 필요 시 수정
     limit=None,                   # 필요 시 숫자
-    title="straight",          # 시트 좌측 첫 열 제목
-    output_csv_path="p21_straight_002_similarity_matrix.csv"
+    title="hook_left",          # 시트 좌측 첫 열 제목
+    output_csv_path="p05_hook_left_004_similarity_matrix.csv"
     )
     
     # 'standard' | 'minmax' | None
