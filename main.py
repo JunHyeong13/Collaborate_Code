0fab44e2f3abb677de62442bd576cbe1e35d0dc8
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-09 00:46:52 +0900

    files upload

--- main.py
+++ main.py
@@ -1060,7 +1060,7 @@ if __name__ == "__main__":
     # straight_003.csv
     
     file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/uppercut_left_004.csv"
-    file2 = "/Users/jonabi/Downloads/TEPA/p16_Global"
+    file2 = "/Users/jonabi/Downloads/TEPA/p13_Global"
     
     # 윈도우 기준 
     #file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\straight_004.csv"
@@ -1105,7 +1105,7 @@ if __name__ == "__main__":
     keyword="uppercut_left",      # 필요 시 수정
     limit=None,                   # 필요 시 숫자
     title="uppercut_left",          # 시트 좌측 첫 열 제목
-    output_csv_path="p16_uppercut_left_004_similarity_matrix.csv"
+    output_csv_path="p13_uppercut_left_004_similarity_matrix.csv"
     )
     
     # 'standard' | 'minmax' | None
