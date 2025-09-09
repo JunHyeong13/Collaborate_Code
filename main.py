d670c852d69913aede159d3c90a047d0c445cadb
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-09 23:41:13 +0900

    files uploaded

--- main.py
+++ main.py
@@ -1059,14 +1059,14 @@ if __name__ == "__main__":
     # jap_001.csv
     # straight_003.csv
     
-    file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/uppercut_left_004.csv"
-    file2 = "/Users/jonabi/Downloads/TEPA/p07_Global"
+    #ile1 = "/Users/jonabi/Downloads/TEPA/mocap_test/uppercut_right_004.csv"
+    #file2 = "/Users/jonabi/Downloads/TEPA/p04_Global"
     #file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/uppercut_left_004.csv"
     #file2 = "/Users/jonabi/Downloads/TEPA/p13_Global"
         
     # 윈도우 기준 
-    #file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\hook_right_002.csv"
-    #file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p18_Global"
+    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\uppercut_left_004.csv"
+    file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p05_Global"
  
 
     # 실행 중 어떤 파일을 비교하는지 표시
@@ -1104,12 +1104,10 @@ if __name__ == "__main__":
     file1_path=file1,
     file2_dir=file2,
     analyzer=analyzer,
-    keyword="hook_right",      # 필요 시 수정
+    keyword="uppercut_right",      # 필요 시 수정
     limit=None,                   # 필요 시 숫자
-    title="uppercut_left",          # 시트 좌측 첫 열 제목
-    output_csv_path="p07_uppercut_left_004_similarity_matrix.csv"
-    title="hook_right",          # 시트 좌측 첫 열 제목
-    output_csv_path="p18_hook_right_002_similarity_matrix.csv"
+    title="uppercut_right",          # 시트 좌측 첫 열 제목
+    output_csv_path="p05_hook_right_004_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
     )
     
     # 'standard' | 'minmax' | None
