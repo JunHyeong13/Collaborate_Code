84faa1cbd6500ed96b8338ffe635cfc4cbc486f4
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-10 15:51:18 +0900

    upload

--- main.py
+++ main.py
@@ -1068,8 +1068,8 @@ if __name__ == "__main__":
     #file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\uppercut_left_004.csv"
     #file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p04_Global"
  
-    file1 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\mocap_test\\uppercut_left_002.csv"
-    file2 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\p21_Global"
+    file1 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\mocap_test\\hook_right_004.csv"
+    file2 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\p19_Global"
  
 
     # 실행 중 어떤 파일을 비교하는지 표시
@@ -1107,10 +1107,10 @@ if __name__ == "__main__":
     file1_path=file1,
     file2_dir=file2,
     analyzer=analyzer,
-    keyword="uppercut_left",      # 필요 시 수정
+    keyword="hook_right",      # 필요 시 수정
     limit=None,                   # 필요 시 숫자
-    title="uppercut_left",          # 시트 좌측 첫 열 제목
-    output_csv_path="p21_uppercut_left_002_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
+    title="hook_right",          # 시트 좌측 첫 열 제목
+    output_csv_path="p19_hook_right_004_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
     )
     
     # 'standard' | 'minmax' | None
