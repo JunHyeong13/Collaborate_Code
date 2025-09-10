a392e9ac7b35fc8efad8071079bb2ebefde4c339
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-10 18:57:57 +0900

    upload

--- main.py
+++ main.py
@@ -1061,15 +1061,15 @@ if __name__ == "__main__":
     
     #ile1 = "/Users/jonabi/Downloads/TEPA/mocap_test/uppercut_right_004.csv"
     #file2 = "/Users/jonabi/Downloads/TEPA/p04_Global"
-    #file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/uppercut_left_004.csv"
-    #file2 = "/Users/jonabi/Downloads/TEPA/p13_Global"
+    file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/uppercut_left_002.csv"
+    file2 = "/Users/jonabi/Downloads/TEPA/p08_Global"
         
     # 윈도우 기준 
     #file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\uppercut_left_004.csv"
     #file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p04_Global"
  
-    file1 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\mocap_test\\uppercut_left_002.csv"
-    file2 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\p21_Global"
+    #file1 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\mocap_test\\uppercut_left_002.csv"
+    #file2 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\p06_Global"
  
 
     # 실행 중 어떤 파일을 비교하는지 표시
@@ -1110,7 +1110,7 @@ if __name__ == "__main__":
     keyword="uppercut_left",      # 필요 시 수정
     limit=None,                   # 필요 시 숫자
     title="uppercut_left",          # 시트 좌측 첫 열 제목
-    output_csv_path="p21_uppercut_left_002_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
+    output_csv_path="p08_uppercut_left_002_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
     )
     
     # 'standard' | 'minmax' | None
