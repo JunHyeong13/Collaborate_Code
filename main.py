36f5fb7f3070c6bf7e5dcf718f5b96ce8dacd7fe
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-11 23:04:27 +0900

    files uploaded

--- main.py
+++ main.py
@@ -1068,8 +1068,8 @@ if __name__ == "__main__":
     #file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\uppercut_left_004.csv"
     #file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p04_Global"
  
-    file1 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\mocap_test\\jap_001.csv"
-    file2 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\p04_Global"
+    file1 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\mocap_test\\hook_left_003.csv"
+    file2 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\p06_Global"
  
 
     # 실행 중 어떤 파일을 비교하는지 표시
@@ -1107,8 +1107,8 @@ if __name__ == "__main__":
     analyzer=analyzer,
     keyword="hook_left",      # 필요 시 수정
     limit=None,                   # 필요 시 숫자
-    title="hook_right",          # 시트 좌측 첫 열 제목
-    output_csv_path="p19_hook_right_004_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
+    title="hook_left",          # 시트 좌측 첫 열 제목
+    output_csv_path="p06_hook_left_003_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
     )
     
     # 'standard' | 'minmax' | None
