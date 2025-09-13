78c27200f13c880af4f593bc097d3352deb9e6c8
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-14 01:16:38 +0900

    upload

--- main.py
+++ main.py
@@ -1059,13 +1059,13 @@ if __name__ == "__main__":
     # jap_001.csv
     # straight_003.csv
 
-    file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/uppercut_right_003.csv"
-    file2 = "/Users/jonabi/Downloads/TEPA/p15_Global"
+    #file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/uppercut_left_001.csv"
+    #file2 = "/Users/jonabi/Downloads/TEPA/p10_Global"
     
     # 윈도우 기준 
 
-    #file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\uppercut_left_001.csv"
-    #file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p09_Global"
+    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\uppercut_left_001.csv"
+    file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p13_Global"
  
     #file1 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\mocap_test\\hook_left_001.csv"
     #file2 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\p25_Global"
@@ -1104,8 +1104,8 @@ if __name__ == "__main__":
         analyzer=analyzer,
         keyword="uppercut_left",      # 필요 시 수정
         limit=None,                   # 필요 시 숫자
-        title="uppercut_right",          # 시트 좌측 첫 열 제목
-        output_csv_path="p15_uppercut_right_003_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
+        title="uppercut_left",          # 시트 좌측 첫 열 제목
+        output_csv_path="p13_uppercut_left_001_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
     )
     
     # 'standard' | 'minmax' | None
