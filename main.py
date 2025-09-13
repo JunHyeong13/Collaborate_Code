ec0450aa56f307abe0fbbc5aae39e79c9c090e88
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-14 03:24:35 +0900

    files upload

--- main.py
+++ main.py
@@ -1052,19 +1052,19 @@ if __name__ == "__main__":
     print("복싱 동작 DTW 분석기 (v2.14 - 안정성/정확도 강화)")
     print("=" * 64)
 
-    # uppercurt_left_001.csv
+    # uppercut_left_001.csv
     # uppercut_right_002.csv
     # hook_left_002.csv
     # hook_right_001.csv
     # jap_001.csv
     # straight_003.csv
 
-    file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/hook_left_004.csv"
-    file2 = "/Users/jonabi/Downloads/TEPA/p23_Global"
+    file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/uppercut_right_003.csv"
+    file2 = "/Users/jonabi/Downloads/TEPA/p21_Global"
     
     # 윈도우 기준 
-    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\uppercut_left_001.csv"
-    file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p15_Global"
+    #file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\uppercut_left_001.csv"
+    #file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p15_Global"
  
     #file1 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\mocap_test\\jap_005.csv"
     #file2 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\p26_Global"
@@ -1101,10 +1101,10 @@ if __name__ == "__main__":
         file1_path=file1,
         file2_dir=file2,
         analyzer=analyzer,
-        keyword="uppercut_left",      # 필요 시 수정
+        keyword="uppercut_right",      # 필요 시 수정
         limit=None,                   # 필요 시 숫자
-        title="hook_left",          # 시트 좌측 첫 열 제목
-        output_csv_path="p23_hook_left_004_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
+        title="uppercut_right",          # 시트 좌측 첫 열 제목
+        output_csv_path="p21_uppercut_right_003_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
     )
     
     # 'standard' | 'minmax' | None
