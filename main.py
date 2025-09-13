9dc962c6428425f5f313a6aff9ac7849a440e356
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-14 03:48:49 +0900

    files

--- main.py
+++ main.py
@@ -1059,8 +1059,8 @@ if __name__ == "__main__":
     # jap_001.csv
     # straight_003.csv
 
-    file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/uppercut_right_003.csv"
-    file2 = "/Users/jonabi/Downloads/TEPA/p21_Global"
+    file1 = "/Users/jonabi/Downloads/TEPA/mocap_test/uppercut_right_005.csv"
+    file2 = "/Users/jonabi/Downloads/TEPA/p23_Global"
     
     # 윈도우 기준 
     #file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\uppercut_left_001.csv"
@@ -1104,7 +1104,7 @@ if __name__ == "__main__":
         keyword="uppercut_right",      # 필요 시 수정
         limit=None,                   # 필요 시 숫자
         title="uppercut_right",          # 시트 좌측 첫 열 제목
-        output_csv_path="p21_uppercut_right_003_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
+        output_csv_path="p23_uppercut_right_005_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
     )
     
     # 'standard' | 'minmax' | None
