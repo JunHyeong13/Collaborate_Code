5669e6b5b31d206333d56a63203407ae46488611
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-14 00:26:35 +0900

    files upload

--- main.py
+++ main.py
@@ -1065,11 +1065,11 @@ if __name__ == "__main__":
     
     # 윈도우 기준 
 
-    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\jap_005.csv"
-    file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p04_Global"
+    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\uppercut_left_001.csv"
+    file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p09_Global"
  
-    file1 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\mocap_test\\hook_left_001.csv"
-    file2 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\p25_Global"
+    # file1 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\mocap_test\\hook_left_001.csv"
+    # file2 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\p25_Global"
  
     # 실행 중 어떤 파일을 비교하는지 표시
     # print(f"분석 대상 파일 1: {file1}")
@@ -1103,10 +1103,10 @@ if __name__ == "__main__":
         file1_path=file1,
         file2_dir=file2,
         analyzer=analyzer,
-        keyword="jap",      # 필요 시 수정
+        keyword="uppercut_left",      # 필요 시 수정
         limit=None,                   # 필요 시 숫자
-        title="jap",          # 시트 좌측 첫 열 제목
-        output_csv_path="p04_jap_005_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
+        title="uppercut_left",          # 시트 좌측 첫 열 제목
+        output_csv_path="p09_uppercut_left_001_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
     )
     
     # 'standard' | 'minmax' | None
