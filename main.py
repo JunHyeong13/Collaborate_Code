78394088038c5494f99e1b74be6c6f6d737a4795
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2025-09-19 01:25:20 +0900

    files uploaded

--- main.py
+++ main.py
@@ -1063,8 +1063,8 @@ if __name__ == "__main__":
     #file2 = "/Users/jonabi/Downloads/TEPA/p06_Global"
     
     # 윈도우 기준 
-    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\hook_right_005.csv"
-    file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p26_Global"
+    file1 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\mocap_test\\hook_left_002.csv"
+    file2 = "C:\\Users\\PC\\Documents\\GitHub\\Collaborate_Code\\p17_Global"
  
     #file1 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\mocap_test\\jap_005.csv"
     #file2 = "C:\\Users\\user\\Downloads\\TEPA\\Collaborate_Code\\p26_Global"
@@ -1101,10 +1101,10 @@ if __name__ == "__main__":
         file1_path=file1,
         file2_dir=file2,
         analyzer=analyzer, # 필요 시 수정
-        keyword="hook_right",      # 필요 시 수정
+        keyword="hook_left",      # 필요 시 수정
         limit=None,                   # 필요 시 숫자
-        title="hook_right",          # 시트 좌측 첫 열 제목
-        output_csv_path="p26_hook_right_005_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
+        title="hook_left",          # 시트 좌측 첫 열 제목
+        output_csv_path="p17_hook_left_002_new_similarity_matrix.csv"      # 시트 좌측 첫 열 제목
     )
     
     # 'standard' | 'minmax' | None
